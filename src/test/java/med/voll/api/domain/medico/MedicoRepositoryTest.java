5a56ba6d089b3368d568675c09f3fe0670c24e89
Fernando Destro Reame <CIANDT\fdestro@lnb021751cps.ciandt.global>
2023-04-10 08:17:14 -0300

    Adicionando environments de prod

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+
+
+    }
+}
